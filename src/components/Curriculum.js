4b1949c5c1b51d2088d29c18c7bef5527ac24e10
David Ortiz <90852224+Sistemshdortiz@users.noreply.github.com>
2023-10-10 01:22:14 +0200

    Update Curriculum.js

--- src/components/Curriculum.js
+++ src/components/Curriculum.js
@@ -4,10 +4,10 @@ import myResumePDF from '../cv/Hernán_Ortiz_Jinez_CV.pdf';
 export const Curriculum = () => {
   return (
     <div className='page'>
-      <h1 className='heading'>Curriculum de Hernán David Ortiz</h1>
+      <h1 className='heading'>Cv</h1>
 
       {/* Mostrar el PDF directamente en la página */}
-      <embed src={myResumePDF} type="application/pdf" width="100%" height="500px" />
+      <embed src={myResumePDF} type="application/pdf" width="100%" height="600px" />
     </div>
   );
 };
