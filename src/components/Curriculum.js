f194cb3711c19224bb80e4371846178d0baf9736
David Ortiz <90852224+Sistemshdortiz@users.noreply.github.com>
2024-01-18 13:52:30 +0100

    Actualización de cvs en componente cv
    Se añaden dos cvs actualizados, uno en inglés y otro en español, además de dos botones para renderizar cada uno de ellos.

--- src/components/Curriculum.js
+++ src/components/Curriculum.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import myResumePDF from '../cv/Hernán_Ortiz_Jinez_CV.pdf';
+import myResumePDF from '../cv/Hernán Ortiz Jinez_es.pdf';
+import myResumePDF2 from '../cv/Hernán Ortiz Jinez_en.pdf';
+
 
 export const Curriculum = () => {
+
+  const [currentPDF, setCurrentPDF] = React.useState(myResumePDF);
+
+  const handleButtonClick = (pdf) => {
+    setCurrentPDF(pdf);
+  };
   return (
     <div className='page'>
       <h1 className='heading'>Cv</h1>
 
       {/* Mostrar el PDF directamente en la página */}
-      <embed src={myResumePDF} type="application/pdf" width="100%" height="600px" />
+      <embed src={currentPDF} type="application/pdf" width="100%" height="600px" />
+      {/* Botones para cambiar el PDF */}
+      <button onClick={() => handleButtonClick(myResumePDF)}>CV en Español</button>
+      <button onClick={() => handleButtonClick(myResumePDF2)}>Cv en Inglés</button>
     </div>
   );
 };
