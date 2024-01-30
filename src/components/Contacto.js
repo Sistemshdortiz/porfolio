ec0d6283cfdcaf91d04949a55e38b14ca9a7e482
David Ortiz <90852224+Sistemshdortiz@users.noreply.github.com>
2024-01-30 01:36:12 +0100

    Update footer and body responsive

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -2,28 +2,25 @@ import React from 'react'
 
 export const Contacto = () => {
   return (
-    <div className="container w-50 bg-secondary mt-5 rounded shadow">
-      <form className='text-center' data-netlify="true" method="POST">
-        <h1 className='fw-bold text-center py-5'>Contacto</h1>
-
+    <div>
+      <h2 className='text-center'>Contacto</h2>
+      <p className='text-center h6'>
+        ¡Gracias por visitar mi página! Si deseas contactarme, puedes hacerlo a través del siguiente formulario o mediante el enlace de mi LinkedIn abajo expuesto.
+      </p>
+    <div className="container   bg-secondary mt-5 rounded shadow" style={{ minWidth: '250px', maxWidth: '30%'}}>
+      <form className='text-center' data-netlify="true" method="POST" netlify >
+        <fieldset>
+          <legend className='text-uppercase font-weight-bold'>¿Hablamos?</legend>
+        </fieldset>
         <div className='mb-3'>
           <input className="form-control" type='text' placeholder='Nombre' name='nombre' autoComplete="true" />
         </div>
-
         <div className='mb-3'>
-          <input type='text' className='form-control' placeholder='Apellidos' name='apellidos' />
+          <input type='email' className='form-control' placeholder='Email' name='email' />
         </div>
-        <div className='mb-3'>
-          <input type='text' className='form-control' placeholder='Email' name='email' />
-        </div>
-        <div className='mb-3'>
-          <textarea type='text' className='form-control' placeholder='Motivo de contacto' name='motivo' />
-        </div>
-
         <input type='submit' value='Enviar' className='mb-3' />
-
       </form>
     </div>
-
+    </div>
   )
 }
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import '@fortawesome/fontawesome-free/css/all.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export const Footer = () => {
   return (
+    // <footer className='footer'>
+    //   <p>Hernán David Ortiz Jinez WEB &copy;2024 Porfolio</p>
+    // </footer>
     <footer className='footer'>
-      <p>Hernán David Ortiz Jinez WEB &copy;2024 Porfolio</p>
+      <div className="container text-center">
+        <p>&copy; {new Date().getFullYear()} Hernán David Ortiz J. | Porfolio</p>
+        <div className="social-icons mt-3">
+          <a href="https://www.linkedin.com/in/h%C3%A9rnan-david-ortiz-jinez-61aa52a5/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} size="3x" style={{ marginRight: '10px' }} />
+          </a>
+          <a href="https://github.com/Sistemshdortiz" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} size="3x" style={{ marginRight: '10px' }} />
+          </a>
+          {/* Agrega más iconos de redes sociales según tus necesidades */}
+        </div>
+      </div>
     </footer>
   )
 }
